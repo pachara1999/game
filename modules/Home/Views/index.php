c8c6a53689992bc43da127595c5892d161d05384
Jitapa Porsom <fernloveluhan@gmail.com>
2023-03-01 15:17:53 +0700

    เพิ่มแนพ

--- modules/Home/Views/index.php
+++ modules/Home/Views/index.php
@@ -1,15 +1,87 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>game</title>
+
 </head>
 
-<body>
-    test
-</body>
+<body style="background-image: linear-gradient(to bottom, #3f2416, #482f22, #503a30, #58463d, #60524b); background-attachment:fixed">
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <span class="navbar-brand mb-0 h1">LOGO</span>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">HOME <br>หน้าแรก </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {}" href="#">NEWS<br>ข่าวสาร</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">INTRODUCTION<br>รูปแบบเกมส์</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">GAMEGIUDE<br>แนะนำการเล่น</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">RANKING<br>จัดอันดับ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">DOWNLOAD<br>ดาวน์โหลดเกมส์</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">MEMBER<br>ระบบสมาชิก</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="text-center">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://img.freepik.com/free-vector/flat-design-abstract-geometric-real-estate-facebook-cover_23-2149184148.jpg?w=996&t=st=1677639871~exp=1677640471~hmac=580a5e85205fd8f15e8e2a57f45d204abce2d9d7959a6bdfc32b6a55d2f70911" width="1500" height="350" class="mw-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://img.freepik.com/free-vector/flat-science-facebook-cover-template_23-2149051278.jpg?w=996&t=st=1677639877~exp=1677640477~hmac=b0e86efb59d7804fc7596b1f1b7d6d1b07ca5a81324b880f2426a3ee58a2d32d" width="1600" height="350" class="mw-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://img.freepik.com/free-vector/flat-science-facebook-cover-template_23-2149051278.jpg?w=996&t=st=1677639877~exp=1677640477~hmac=b0e86efb59d7804fc7596b1f1b7d6d1b07ca5a81324b880f2426a3ee58a2d32d" width="1600" height="350" class="mw-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <body>
+        <div class="container">
+            <img src="https://as1.ftcdn.net/v2/jpg/05/73/46/90/1000_F_573469043_ymmXxeP22NGRQ63ehBzKJPBoAZ0tAWZV.jpg" width="200" height="100" class="rounded float-start" alt="...">
+            <div style="background-color: #3C2A21; width: 200px; height: 300px;">
+            </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+    </body>
 
 </html>

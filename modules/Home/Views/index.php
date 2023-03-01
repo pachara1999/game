ade70749ad931852dcf78fc6a77d8ccea240ac38
Jitapa Porsom <fernloveluhan@gmail.com>
2023-03-01 20:42:16 +0700

    Update index.php

--- modules/Home/Views/index.php
+++ modules/Home/Views/index.php
@@ -13,31 +13,31 @@
 
 </head>
 
-<body style="background-image: linear-gradient(to bottom, #3f2416, #482f22, #503a30, #58463d, #60524b); background-attachment:fixed">
-    <nav class="navbar navbar-dark bg-dark">
+<body style="background-image: linear-gradient(to bottom, #1a120b, #211812, #2a1e17, #33241c, #3c2a21); background-attachment:fixed">
+    <nav class="navbar navbar-dark bg-dark text-light">
         <div class="container">
             <span class="navbar-brand mb-0 h1">LOGO</span>
             <ul class="nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">HOME <br>หน้าแรก </a>
+                <li class="nav-item text-center">
+                    <a class="nav-link menunav" href="#">HOME <br>หน้าแรก </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {}" href="#">NEWS<br>ข่าวสาร</a>
+                <li class="nav-item text-center">
+                    <a class="nav-link menunav" href="#">NEWS<br>ข่าวสาร</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">INTRODUCTION<br>รูปแบบเกมส์</a>
+                <li class="nav-item text-center">
+                    <a class="nav-link menunav" href="#">INTRODUCTION<br>รูปแบบเกมส์</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">GAMEGIUDE<br>แนะนำการเล่น</a>
+                <li class="nav-item text-center">
+                    <a class="nav-link menunav" href="#">GAMEGIUDE<br>แนะนำการเล่น</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">RANKING<br>จัดอันดับ</a>
+                <li class="nav-item text-center">
+                    <a class="nav-link menunav" href="#">RANKING<br>จัดอันดับ</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">DOWNLOAD<br>ดาวน์โหลดเกมส์</a>
+                <li class="nav-item text-center">
+                    <a class="nav-link menunav" href="#">DOWNLOAD<br>ดาวน์โหลดเกมส์</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">MEMBER<br>ระบบสมาชิก</a>
+                <li class="nav-item text-center">
+                    <a class="nav-link menunav" href="#">MEMBER<br>ระบบสมาชิก</a>
                 </li>
             </ul>
         </div>
@@ -52,13 +52,13 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="https://img.freepik.com/free-vector/flat-design-abstract-geometric-real-estate-facebook-cover_23-2149184148.jpg?w=996&t=st=1677639871~exp=1677640471~hmac=580a5e85205fd8f15e8e2a57f45d204abce2d9d7959a6bdfc32b6a55d2f70911" width="1500" height="350" class="mw-100" alt="...">
+                        <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="1500" height="350" class="mw-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="https://img.freepik.com/free-vector/flat-science-facebook-cover-template_23-2149051278.jpg?w=996&t=st=1677639877~exp=1677640477~hmac=b0e86efb59d7804fc7596b1f1b7d6d1b07ca5a81324b880f2426a3ee58a2d32d" width="1600" height="350" class="mw-100" alt="...">
+                        <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="1600" height="350" class="mw-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="https://img.freepik.com/free-vector/flat-science-facebook-cover-template_23-2149051278.jpg?w=996&t=st=1677639877~exp=1677640477~hmac=b0e86efb59d7804fc7596b1f1b7d6d1b07ca5a81324b880f2426a3ee58a2d32d" width="1600" height="350" class="mw-100" alt="...">
+                        <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="1600" height="350" class="mw-100" alt="...">
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -75,11 +75,204 @@
 
     <body>
         <div class="container">
-            <img src="https://as1.ftcdn.net/v2/jpg/05/73/46/90/1000_F_573469043_ymmXxeP22NGRQ63ehBzKJPBoAZ0tAWZV.jpg" width="200" height="100" class="rounded float-start" alt="...">
-            <div style="background-color: #3C2A21; width: 200px; height: 300px;">
-            </div>
+            <div class="row align-items-start">
+                <div class="my-2 col-md-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="100" class="mw-100" alt="...">
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div s class="col-12">
+                                        <h6>Username</h6>
+                                        <input type="text" class="my-1 form-control" placholder="Username" />
+                                    </div>
+                                    <div class="col-12">
+                                        <h6>Password</h6>
+                                        <input type="password" class="my-1 form-control" placholder="Username" />
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submidt" class="my-1 w-50 btn" style="background-color: #8B7E74">Login</button>
+                                        <button type="submidt" class="my-1 w-50 btn" style="background-color: #C7BCA1">Register</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <h6>HIGHLIGHT</h6>
+                                <iframe width="250" height="200" src="https://www.youtube.com/embed/q3bWhsg9HH8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card" style="height: 100px;">
+                            <div class="card-body">
+                                <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="70" class="mw-100" alt="...">
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                <div class="my-2 col-md-6">
+                    <div class="card" style="height: 60px;">
+                        <div class="card-body">
+                            WELCOME!!!
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card" style="height: 280px;">
+                            <div class="card-body text-center">
+                                <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="700" height="250" class="mw-100" alt="...">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card" style="height: 250px;">
+                            <div class="card-body">
+                                <div class="col">
+                                    news to day
+                                </div>
+                                <div class="col">
+                                    news 1*******************
+                                </div>
+                                <div class="col">
+                                    news 2*******************
+                                </div>
+                                <div class="col">
+                                    news 3*******************
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card" style="height: 420px;">
+                            <div class="card-body">
+                                <div class="my-2">
+                                    <div class="col">
+                                        RECENT NEWS
+                                    </div>
+                                </div>
+                                <div class="my-2">
+                                    <div class="col">
+                                        <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="150" height="100" class="mw-100" alt="...">
+                                        Promotion 1
+                                    </div>
+                                </div>
+                                <div class="my-2">
+                                    <div class="col">
+                                        <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="150" height="100" class="mw-100" alt="...">
+                                        Promotion 2
+                                    </div>
+                                </div>
+                                <div class="my-2">
+                                    <div class="col">
+                                        <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="150" height="100" class="mw-100" alt="...">
+                                        Promotion 3
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="my-2 col-md-3">
+                    <div class="card" style="height: 580px;">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                CLASS LIST
+                            </div>
+                        </div>
+                        <div class="my-2 col-md-12">
+                            <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>
+                            <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>
+                            <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>
+                            <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>
+                            <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>
+                            <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card" style="height: 100px;">
+                            <div class="card-body">
+                                <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="70" class="mw-100" alt="...">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-2">
+                        <div class="card" style="height: 290px;">
+                            <div class="card-body text-center">
+                            <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>   <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>   <div class="my-1">
+                                <div class="col" style="text-align: center;">
+                                    <img src="https://img.freepik.com/free-vector/green-watercolor-beige-background_53876-114427.jpg?w=996&t=st=1677661699~exp=1677662299~hmac=093cc2da12cddf094934a45e8a76e971b4fe732953472b81c9a5b83b16b28a6d" width="290" height="80" class="mw-100" alt="...">
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            </template>
+
+
+            <!-- css -->
+            <style>
+                .navbar {
+                    background-color: #0e0601;
+                }
+
+                .menunav {
+                    font-size: 13px;
+                    color: white;
+                }
+
+                .card {
+
+                    background-color: #E5E5CB;
+                }
+            </style>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
     </body>
